f490edd0519d71b23a7410cfa592e0d5fa4f9467
vsay <vsayfb@gmail.com>
2022-05-22 08:56:25 -0400

    npm i swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog Platform API')
+    .setDescription('An API for your frontend blog projects.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
