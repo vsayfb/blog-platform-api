8764557c1da1390aa1500f3a65b9a48827b9503a
vsay <vsayfb@gmail.com>
2022-05-30 19:01:39 -0400

    modified exists methods in acc:repo

--- src/accounts/accounts.repository.ts
+++ src/accounts/accounts.repository.ts
@@ -14,12 +14,12 @@ export class AccountsRepository {
     return this.accountRepository.save(data);
   }
 
-  existsByUsername(username: string) {
-    return this.accountRepository.findOne({ username });
+  async existsByUsername(username: string) {
+    return !!(await this.accountRepository.findOne({ username }));
   }
 
-  existsByEmail(email: string) {
-    return this.accountRepository.findOne({ email });
+  async existsByEmail(email: string) {
+    return !!(await this.accountRepository.findOne({ email }));
   }
 
   async findByUsernameOrEmail(usernameOrEmail: string) {
