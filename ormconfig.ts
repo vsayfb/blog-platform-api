a4f1a79179a9da08f95e4ce13dfebf09bb45794c
vsay <vsayfb@gmail.com>
2022-06-24 13:02:13 -0400

    prod bug -> removed datasource class added typeorm module options

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,24 +1,21 @@
 import * as dotenv from 'dotenv';
-import { DataSource } from 'typeorm';
 import { env } from 'process';
 import { join } from 'path';
 import { DEV_DATABASE, NODE_ENV, PROD_DATABASE } from 'src/lib/env';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 dotenv.config();
 
 const productionEnviroment = env[NODE_ENV] === 'production';
 
-export const dataSource = new DataSource({
+export const config: TypeOrmModuleOptions = {
   type: 'postgres',
   url: productionEnviroment ? env[PROD_DATABASE] : env[DEV_DATABASE],
   entities: [join(__dirname, '**', '*.entity.{ts,js}')],
   migrations: [join(__dirname + '/src/database/migrations/*.{ts,js}')],
-  ssl: false,
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
   migrationsRun: productionEnviroment,
   synchronize: !productionEnviroment,
-});
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSource } from 'ormconfig';
+import { config } from 'ormconfig';
 import { DatabaseService } from './database.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSource.options)],
+  imports: [TypeOrmModule.forRoot(config)],
   providers: [DatabaseService],
 })
 export class DatabaseModule {}
