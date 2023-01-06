8c1dc891ece811dd7cacb72c2bacea40cef20603
vsay <96926401+vsayfb@users.noreply.github.com>
2023-01-06 16:06:00 -0500

    Dockerize app && Create Workflow

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,18 @@ import * as express from 'express';
 import * as fs from 'fs';
 import { useContainer } from 'class-validator';
 
-function createReverseProxyServer() {
-  const server = express();
-
-  server.listen(80);
-
-  server.get('*', (req, res) =>
-    res.redirect(process.env.SECURE_HOST + req.url),
-  );
-}
-
 async function bootstrap() {
   const nestAppOptions: NestApplicationOptions = {};
 
   if (process.env.NODE_ENV === 'production') {
+    const server = express();
+
+    server.listen(80);
+
+    server.get('*', (req, res) =>
+      res.redirect(process.env.SECURE_HOST + req.url),
+    );
+
     nestAppOptions.httpsOptions = {
       key: fs.readFileSync(process.env.SSL_KEY_PATH, 'utf8'),
       cert: fs.readFileSync(process.env.SSL_CERT_PATH, 'utf8'),
@@ -30,8 +28,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, nestAppOptions);
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-
-  if (process.env.NODE_ENV === 'production ') createReverseProxyServer();
 
   const globalValidationPipe = new ValidationPipe({
     forbidUnknownValues: true,
